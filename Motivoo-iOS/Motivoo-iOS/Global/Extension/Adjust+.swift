36a0a7e3981ba1b775cd001dc8c76b0908113755
binisnil <dbsqlssss0704@gmail.com>
2023-12-29 16:26:06 +0900

    [Setting] #1 - adjusted extension 추가

---
+++ Motivoo-iOS/Motivoo-iOS/Global/Extension/Adjust+.swift
@@ -0,0 +1,62 @@
+//
+//  Adjust+.swift
+//  Motivoo-iOS
+//
+//  Created by 박윤빈 on 2023/12/29.
+//
+
+import UIKit
+
+extension CGFloat {
+    var adjusted: CGFloat {
+        let ratio: CGFloat = UIScreen.main.bounds.width / 375
+        let ratioH: CGFloat = UIScreen.main.bounds.height / 667
+        return ratio <= ratioH ? self * ratio : self * ratioH
+    }
+    
+    var adjustedWidth: CGFloat {
+        let ratio: CGFloat = UIScreen.main.bounds.width / 375
+        return CGFloat(self) * ratio
+    }
+    
+    var adjustedHeight: CGFloat {
+        let ratio: CGFloat = UIScreen.main.bounds.height / 667
+        return CGFloat(self) * ratio
+    }
+}
+
+extension Int {
+    var adjusted: CGFloat {
+        let ratio: CGFloat = UIScreen.main.bounds.width / 375
+        let ratioH: CGFloat = UIScreen.main.bounds.height / 667
+        return ratio <= ratioH ? CGFloat(self) * ratio : CGFloat(self) * ratioH
+    }
+    
+    var adjustedWidth: CGFloat {
+        let ratio: CGFloat = UIScreen.main.bounds.width / 375
+        return CGFloat(self) * ratio
+    }
+    
+    var adjustedHeight: CGFloat {
+        let ratio: CGFloat = UIScreen.main.bounds.height / 667
+        return CGFloat(self) * ratio
+    }
+}
+
+extension Double {
+    var adjusted: CGFloat {
+        let ratio: CGFloat = UIScreen.main.bounds.width / 375
+        let ratioH: CGFloat = UIScreen.main.bounds.height / 667
+        return ratio <= ratioH ? CGFloat(self) * ratio : CGFloat(self) * ratioH
+    }
+    
+    var adjustedWidth: CGFloat {
+        let ratio: CGFloat = UIScreen.main.bounds.width / 375
+        return CGFloat(self) * ratio
+    }
+    
+    var adjustedHeight: CGFloat {
+        let ratio: CGFloat = UIScreen.main.bounds.height / 667
+        return CGFloat(self) * ratio
+    }
+}
