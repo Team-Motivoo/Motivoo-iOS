876e9d88325244c6c0fe5f2f9519e09f80dbd2ed
binisnil <dbsqlssss0704@gmail.com>
2024-01-04 02:02:35 +0900

    [Chore] 디자인시스템 폰트 크기에 .adjusted 추가

--- Motivoo-iOS/Motivoo-iOS/Global/Extension/UIFont+.swift
+++ Motivoo-iOS/Motivoo-iOS/Global/Extension/UIFont+.swift
@@ -9,58 +9,58 @@ import UIKit
 
 extension UIFont{
     class var heading1: UIFont {
-        return UIFont(name: AppFontName.medium.rawValue, size: 48)!
+        return UIFont(name: AppFontName.medium.rawValue, size: 48.adjusted)!
     }
     
     class var heading2: UIFont {
-        return UIFont(name: AppFontName.semiBold.rawValue, size: 24)!
+        return UIFont(name: AppFontName.semiBold.rawValue, size: 24.adjusted)!
     }
     
     class var heading3: UIFont {
-        return UIFont(name: AppFontName.medium.rawValue, size: 24)!
+        return UIFont(name: AppFontName.medium.rawValue, size: 24.adjusted)!
     }
     
     class var heading4: UIFont {
-        return UIFont(name: AppFontName.semiBold.rawValue, size: 18)!
+        return UIFont(name: AppFontName.semiBold.rawValue, size: 18.adjusted)!
     }
     
     class var heading5: UIFont {
-        return UIFont(name: AppFontName.semiBold.rawValue, size: 17)!
+        return UIFont(name: AppFontName.semiBold.rawValue, size: 17.adjusted)!
     }
     
     class var heading6: UIFont {
-        return UIFont(name: AppFontName.semiBold.rawValue, size: 16)!
+        return UIFont(name: AppFontName.semiBold.rawValue, size: 16.adjusted)!
     }
     
     class var body1: UIFont {
-        return UIFont(name: AppFontName.medium.rawValue, size: 18)!
+        return UIFont(name: AppFontName.medium.rawValue, size: 18.adjusted)!
     }
     
     class var body2: UIFont {
-        return UIFont(name: AppFontName.regular.rawValue, size: 18)!
+        return UIFont(name: AppFontName.regular.rawValue, size: 18.adjusted)!
     }
     
     class var body3: UIFont {
-        return UIFont(name: AppFontName.medium.rawValue, size: 17)!
+        return UIFont(name: AppFontName.medium.rawValue, size: 17.adjusted)!
     }
     
     class var body4: UIFont {
-        return UIFont(name: AppFontName.regular.rawValue, size: 17)!
+        return UIFont(name: AppFontName.regular.rawValue, size: 17.adjusted)!
     }
     
     class var body5: UIFont {
-        return UIFont(name: AppFontName.medium.rawValue, size: 16)!
+        return UIFont(name: AppFontName.medium.rawValue, size: 16.adjusted)!
     }
     
     class var body6: UIFont {
-        return UIFont(name: AppFontName.regular.rawValue, size: 16)!
+        return UIFont(name: AppFontName.regular.rawValue, size: 16.adjusted)!
     }
     
     class var caption1: UIFont {
-        return UIFont(name: AppFontName.semiBold.rawValue, size: 15)!
+        return UIFont(name: AppFontName.semiBold.rawValue, size: 15.adjusted)!
     }
     
     class var caption2: UIFont {
-        return UIFont(name: AppFontName.regular.rawValue, size: 15)!
+        return UIFont(name: AppFontName.regular.rawValue, size: 15.adjusted)!
     }
 }
