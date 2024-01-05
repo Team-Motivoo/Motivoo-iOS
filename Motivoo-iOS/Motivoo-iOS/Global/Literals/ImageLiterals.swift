e1308a646548bdf4e6d0d0ab081e1d2228ebc190
binisnil <dbsqlssss0704@gmail.com>
2024-01-05 17:03:07 +0900

    [Setting] #6 - ImageLiterals ima, icon으로 분리

--- Motivoo-iOS/Motivoo-iOS/Global/Literals/ImageLiterals.swift
+++ Motivoo-iOS/Motivoo-iOS/Global/Literals/ImageLiterals.swift
@@ -9,11 +9,18 @@ import UIKit
 
 
 enum ImageLiterals {
-
-    // MARK: - OnBoarding
     
-    static var motivooLogo: UIImage { .load(named: "motivooLogo")}
-    static var kakaoLogin: UIImage { .load(named: "kakaoLogin")}
-    static var appleLogin: UIImage { .load(named: "appleLogin")}
+    enum img {
+        
+        // MARK: - OnBoarding
+        
+        static var motivooLogo: UIImage { .load(named: "motivooLogo")}
+        static var kakaoLogin: UIImage { .load(named: "kakaoLogin")}
+        static var appleLogin: UIImage { .load(named: "appleLogin")}
+    }
+    
+    enum icon {
+        
+    }
 }
 
