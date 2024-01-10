d8c1bd29b36b379d857100e5390fe947b07f2a72
binisnil <dbsqlssss0704@gmail.com>
2024-01-10 22:19:59 +0900

    [Chore] #8 - TextLiterals 추가

--- Motivoo-iOS/Motivoo-iOS/Global/Literals/TextLiterals.swift
+++ Motivoo-iOS/Motivoo-iOS/Global/Literals/TextLiterals.swift
@@ -15,8 +15,15 @@ enum TextLiterals {
     }
 
     enum Home {
-//        static var example = "이런 식으로 텍스트를 추가해주시면 됩니다! 각 열거형 안에 다른 열거형을 만드셔도 좋아요~"
-//        위 주석은 다음 작업때 삭제 예정입니다!
+        static var date = "\(DateLiterals.year)년 \(DateLiterals.month)월 \(DateLiterals.day)일"
+        static var pick = "오늘 할 운동을 골라주세요!"
+        static var firstMission = "8천걸음 걷고\n스쿼트 10번 하기"
+        static var secondMission = "8천걸음 걸어서\n트리 보러가기"
+        static var walk = "걸음 수"
+        static var myWalk = "내 걸음"
+        static var parentWalk = "부모님 걸음"
+        static var clearPercent = "운동 달성률"
+        static var checkMission = "운동 인증하기"
     }
     
     enum MyPage {
