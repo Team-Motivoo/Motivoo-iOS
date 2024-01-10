bacc9c0184d2e50d835dd196d9434b7001bfa064
binisnil <dbsqlssss0704@gmail.com>
2024-01-10 22:17:59 +0900

    [Chore] #8 - 더미데이터를 위한 Model 추가

---
+++ Motivoo-iOS/Motivoo-iOS/Data/Home/Model/HomeStepModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeStepModel.swift
+//  Motivoo-iOS
+//
+//  Created by 박윤빈 on 2024/01/09.
+//
+
+import Foundation
+
+struct HomeStepModel {
+    let myStep: Int
+    let parentStep: Int
+}
