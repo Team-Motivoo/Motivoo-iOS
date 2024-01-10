71aa12be9a2fd04b59b51582715f3587c74cad14
binisnil <dbsqlssss0704@gmail.com>
2024-01-10 22:21:56 +0900

    [Feat] #8 - Home 하단 만보수 표시 UI 구현 완료

---
+++ Motivoo-iOS/Motivoo-iOS/Feature/Home/Views/HomeStepCountView.swift
@@ -0,0 +1,121 @@
+//
+//  HomeStepCountView.swift
+//  Motivoo-iOS
+//
+//  Created by 박윤빈 on 2024/01/09.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class HomeStepCountView: BaseView {
+    
+    // MARK: - Properties
+        
+    // MARK: - UI Components
+    
+    private var myWalkLabel = UILabel()
+    private var parentWalkLabel = UILabel()
+    private var myWalkCountLabel = UILabel()
+    private var parentWalkCountLabel = UILabel()
+    private var divideLineView = UIView()
+    private lazy var myWalkStackView = UIStackView(arrangedSubviews: [myWalkLabel,
+                                                                      myWalkCountLabel])
+    private lazy var parentWalkStackView = UIStackView(arrangedSubviews: [parentWalkLabel,
+                                                                          parentWalkCountLabel])
+    private lazy var totalStackView = UIStackView(arrangedSubviews: [myWalkStackView,
+                                                                     divideLineView,
+                                                                     parentWalkStackView])
+    
+    // MARK: - inits
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Override Functions
+    
+    override func setUI() {
+        self.do {
+            $0.backgroundColor = .gray100
+            $0.layer.cornerRadius = 8
+        }
+        
+        myWalkLabel.do {
+            $0.font = .caption2
+            $0.textColor = .gray600
+            $0.text = TextLiterals.Home.myWalk
+        }
+        
+        parentWalkLabel.do {
+            $0.font = .caption2
+            $0.textColor = .gray600
+            $0.text = TextLiterals.Home.parentWalk
+        }
+        
+        myWalkCountLabel.do {
+            $0.font = .heading2
+            $0.textColor = .blue800
+        }
+        
+        parentWalkCountLabel.do {
+            $0.font = .heading2
+            $0.textColor = .gray600
+        }
+        
+        divideLineView.do {
+            $0.backgroundColor = .gray300
+        }
+        
+        myWalkStackView.do {
+            $0.axis = .vertical
+            $0.spacing = 0
+            $0.alignment = .center
+        }
+        
+        parentWalkStackView.do {
+            $0.axis = .vertical
+            $0.spacing = 0
+            $0.alignment = .center
+        }
+        
+        totalStackView.do {
+            $0.axis = .horizontal
+            $0.spacing = 45.adjusted
+            $0.alignment = .center
+        }
+    }
+    
+    override func setHierachy() {
+        self.addSubview(totalStackView)
+    }
+    
+    override func setLayout() {
+        self.snp.makeConstraints {
+            $0.height.equalTo(104.adjusted)
+        }
+        
+        totalStackView.snp.makeConstraints {
+            $0.center.height.equalToSuperview()
+        }
+
+        divideLineView.snp.makeConstraints {
+            $0.width.equalTo(1.adjusted)
+            $0.top.bottom.equalToSuperview().inset(20.adjusted)
+        }
+    }
+    
+    // MARK: - Custom Functions
+    
+    func configureView(myWalk: Int, parentWalk: Int) {
+        myWalkCountLabel.text = "\(myWalk)"
+        parentWalkCountLabel.text = "\(parentWalk)"
+    }
+}
