25de27a9a75effd77ac9b165185dda37a719a1de
LeeJoEun-01 <joeun1005@naver.com>
2024-01-13 01:27:21 +0900

    [Feat] #17 - OnboardingView UI 구현

---
+++ Motivoo-iOS/Motivoo-iOS/Feature/Onboarding/Views/Cells/OnboardingView4Cell.swift
@@ -0,0 +1,83 @@
+//
+//  OnboardingView4Cell.swift
+//  Motivoo-iOS
+//
+//  Created by 이조은 on 1/12/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class OnboardingView4Cell: UICollectionViewCell {
+    private let bgView = UIView()
+    let question4Title = UILabel()
+    var buttonStackView = UIStackView()
+    let day1Button = MotivooButton(text: TextLiterals.Onboarding.Q3.one, buttonStyle: .borderGray100)
+    let day2Button = MotivooButton(text: TextLiterals.Onboarding.Q3.two, buttonStyle: .borderGray100)
+    let day3Button = MotivooButton(text: TextLiterals.Onboarding.Q3.three, buttonStyle: .borderGray100)
+    let day5Button = MotivooButton(text: TextLiterals.Onboarding.Q3.five, buttonStyle: .borderGray100)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setUI()
+        setHierachy()
+        setLayout()
+        day1Button.isSelected = true
+        day2Button.isSelected = true
+        day3Button.isSelected = true
+        day5Button.isSelected = true
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Override Functions
+
+    func setUI() {
+        question4Title.do {
+            $0.text = TextLiterals.Onboarding.Q3.howManyExercise
+            $0.font = .heading3
+            $0.textColor = .gray900
+            $0.textAlignment = .left
+            $0.numberOfLines = 0
+            $0.addLineHeight(lineHeight: 33.6.adjusted)
+        }
+        buttonStackView.do {
+            $0.axis = .vertical
+            $0.spacing = 16
+            $0.alignment = .center
+            $0.distribution = .fillEqually
+        }
+        day1Button.accessibilityIdentifier = "1day"
+        day2Button.accessibilityIdentifier = "2day"
+        day3Button.accessibilityIdentifier = "3day"
+        day5Button.accessibilityIdentifier = "5day"
+    }
+
+    func setHierachy() {
+        contentView.addSubviews(bgView)
+        bgView.addSubviews(question4Title, buttonStackView)
+        buttonStackView.addArrangedSubviews(day1Button, day2Button, day3Button, day5Button)
+    }
+
+    func setLayout() {
+        bgView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(335.adjusted)
+            $0.height.equalTo(500.adjusted)
+        }
+        question4Title.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(33.6.adjusted)
+            $0.leading.trailing.equalToSuperview()
+        }
+        buttonStackView.snp.makeConstraints {
+            $0.top.equalTo(question4Title.snp.bottom).offset(52.adjusted)
+            $0.leading.equalToSuperview()
+        }
+    }
+}
---
+++ Motivoo-iOS/Motivoo-iOS/Feature/Onboarding/Views/Cells/OnboardingView6Cell.swift
@@ -0,0 +1,148 @@
+//
+//  OnboardingView6Cell.swift
+//  Motivoo-iOS
+//
+//  Created by 이조은 on 1/12/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class OnboardingView6Cell: UICollectionViewCell {
+    private let bgView = UIView()
+    let question6Title = UILabel()
+    let question6Subtitle = UILabel()
+    var buttonStackView = UIStackView()
+    var stackView1 = UIStackView()
+    var stackView2 = UIStackView()
+    var stackView3 = UIStackView()
+    let neckButton = MotivooButton(text: TextLiterals.Onboarding.Q3.neck, buttonStyle: .borderGray100Half)
+    let shoulderButton = MotivooButton(text: TextLiterals.Onboarding.Q3.shoulder, buttonStyle: .borderGray100Half)
+    let waistButton = MotivooButton(text: TextLiterals.Onboarding.Q3.waist, buttonStyle: .borderGray100Half)
+    let kneeButton = MotivooButton(text: TextLiterals.Onboarding.Q3.knee, buttonStyle: .borderGray100Half)
+    let wristButton = MotivooButton(text: TextLiterals.Onboarding.Q3.wrist, buttonStyle: .borderGray100Half)
+    let ankleButton = MotivooButton(text: TextLiterals.Onboarding.Q3.ankle, buttonStyle: .borderGray100Half)
+    let startMotivooButton = MotivooButton(text: TextLiterals.Onboarding.Q3.startMotivoo, buttonStyle: .gray900)
+
+    var isButtonEnabled: Bool = false {
+        didSet {
+            if isButtonEnabled {
+                startMotivooButton.setTitleColor(.white, for: .normal)
+                startMotivooButton.backgroundColor = .gray900
+            } else {
+                startMotivooButton.setTitleColor(.gray400, for: .normal)
+                startMotivooButton.backgroundColor = .gray100
+            }
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierachy()
+        setLayout()
+        neckButton.isSelected = true
+        shoulderButton.isSelected = true
+        waistButton.isSelected = true
+        kneeButton.isSelected = true
+        wristButton.isSelected = true
+        ankleButton.isSelected = true
+        startMotivooButton.isEnabled = isButtonEnabled
+        startMotivooButton.setTitleColor(.gray400, for: .normal)
+        startMotivooButton.backgroundColor = .gray100
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Override Functions
+
+    func setUI() {
+        question6Title.do {
+            $0.text = TextLiterals.Onboarding.Q3.howLongExercise
+            $0.font = .heading3
+            $0.textColor = .gray900
+            $0.textAlignment = .left
+            $0.numberOfLines = 0
+            $0.addLineHeight(lineHeight: 33.6.adjusted)
+        }
+        question6Subtitle.do {
+            $0.text = TextLiterals.Onboarding.Q3.choiceMaxThree
+            $0.font = .body6
+            $0.textColor = .gray600
+            $0.textAlignment = .left
+        }
+        buttonStackView.do {
+            $0.axis = .vertical
+            $0.spacing = 16
+            $0.alignment = .center
+            $0.distribution = .fillEqually
+        }
+        stackView1.do {
+            $0.axis = .horizontal
+            $0.spacing = 12
+            $0.alignment = .fill
+            $0.distribution = .fillEqually
+        }
+        stackView2.do {
+            $0.axis = .horizontal
+            $0.spacing = 12
+            $0.alignment = .fill
+            $0.distribution = .fillEqually
+        }
+        stackView3.do {
+            $0.axis = .horizontal
+            $0.spacing = 12
+            $0.alignment = .fill
+            $0.distribution = .fillEqually
+        }
+        neckButton.accessibilityIdentifier = "neck"
+        shoulderButton.accessibilityIdentifier = "shoulderButton"
+        waistButton.accessibilityIdentifier = "waist"
+        kneeButton.accessibilityIdentifier = "knee"
+        wristButton.accessibilityIdentifier = "wrist"
+        ankleButton.accessibilityIdentifier = "ankle"
+    }
+
+    func setHierachy() {
+        contentView.addSubviews(bgView, startMotivooButton)
+        bgView.addSubviews(question6Title, question6Subtitle, buttonStackView)
+        buttonStackView.addArrangedSubviews(stackView1, stackView2, stackView3)
+        stackView1.addArrangedSubviews(neckButton, shoulderButton)
+        stackView2.addArrangedSubviews(waistButton, kneeButton)
+        stackView3.addArrangedSubviews(wristButton, ankleButton)
+
+    }
+
+    func setLayout() {
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        bgView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(335.adjusted)
+            $0.height.equalTo(500.adjusted)
+        }
+        question6Title.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(33.6.adjusted)
+            $0.leading.trailing.equalToSuperview()
+        }
+        question6Subtitle.snp.makeConstraints {
+            $0.top.equalTo(question6Title.snp.bottom).offset(8.adjusted)
+            $0.leading.equalToSuperview()
+        }
+        buttonStackView.snp.makeConstraints {
+            $0.top.equalTo(question6Subtitle.snp.bottom).offset(52.adjusted)
+            $0.leading.equalToSuperview()
+        }
+        startMotivooButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(78.adjusted)
+        }
+    }
+}
