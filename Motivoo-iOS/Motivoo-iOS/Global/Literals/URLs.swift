108d79aa08f58b99db5c1c9c018673388a3b7616
binisnull <dbsqlssss0704@gmail.com>
2024-01-19 03:52:29 +0900

    [Chore] #38 API URL을 위한 Literals 생성

---
+++ Motivoo-iOS/Motivoo-iOS/Global/Literals/URLs.swift
@@ -0,0 +1,17 @@
+//
+//  URLs.swift
+//  Motivoo-iOS
+//
+//  Created by 박윤빈 on 1/17/24.
+//
+
+import Foundation
+
+enum URLs {
+    enum Home {
+        static var home = "/home"
+        static var mission = "/mission/today/choice"
+        static var missionChoice = "/mission/today"
+        static var missionImage = "/mission/image"
+    }
+}
