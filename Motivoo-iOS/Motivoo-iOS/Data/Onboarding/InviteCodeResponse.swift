5f9daf7ffb8cc8ad73eb6dd00bacc142308b952d
LeeJoEun-01 <joeun1005@naver.com>
2024-03-06 02:05:24 +0900

    [Chore] #71 - InviteCodeResponse 수정

--- Motivoo-iOS/Motivoo-iOS/Data/Onboarding/InviteCodeResponse.swift
+++ Motivoo-iOS/Motivoo-iOS/Data/Onboarding/InviteCodeResponse.swift
@@ -9,14 +9,12 @@ import Foundation
 
 struct InviteCodeResponse: Codable {
     let userId: Int
+    let opponentUserId: Int
     let isMatched: Bool
-    let myInviteCode: Bool
-    let isFinishedOnboarding: Bool
 
     enum CodingKeys: String, CodingKey {
         case userId = "user_id"
+        case opponentUserId = "opponent_user_id"
         case isMatched = "is_matched"
-        case myInviteCode = "my_invite_code"
-        case isFinishedOnboarding = "is_finished_onboarding"
     }
 }
