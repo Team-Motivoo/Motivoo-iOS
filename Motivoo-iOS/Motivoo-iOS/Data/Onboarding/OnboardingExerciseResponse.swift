816e9b5690ab15e8e6edd418ea315be216f010b1
LeeJoEun-01 <joeun1005@naver.com>
2024-03-12 17:01:59 +0900

    [Chore] #71 - ExerciseResponse 수정

--- Motivoo-iOS/Motivoo-iOS/Data/Onboarding/OnboardingExerciseResponse.swift
+++ Motivoo-iOS/Motivoo-iOS/Data/Onboarding/OnboardingExerciseResponse.swift
@@ -9,12 +9,10 @@ import Foundation
 
 struct OnboardingExerciseResponse: Codable {
     let userID: Int
-    let inviteCode: String?
     let exerciseLevel: String
 
     enum CodingKeys: String, CodingKey {
         case userID = "user_id"
-        case inviteCode = "invite_code"
         case exerciseLevel = "exercise_level"
     }
 }
